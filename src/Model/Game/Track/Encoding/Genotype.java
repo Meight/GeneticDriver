fce33e243cbec32cdce605f6c810f437a02a6974
matt.leboucher <matt.leboucher@gmail.com>
2018-03-19 20:59:48 -0400

    Track's genotype holds control points.

--- src/Model/Game/Track/Encoding/Genotype.java
+++ src/Model/Game/Track/Encoding/Genotype.java
@@ -1,7 +1,19 @@
 package Model.Game.Track.Encoding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Matthieu Le Boucher
  */
 public class Genotype {
+    private List<ControlPoint> controlPoints;
+
+    public Genotype() {
+        this.controlPoints = new ArrayList<ControlPoint>();
+    }
+
+    public void addControlPoint(ControlPoint controlPoint) {
+        this.controlPoints.add(controlPoint);
+    }
 }
