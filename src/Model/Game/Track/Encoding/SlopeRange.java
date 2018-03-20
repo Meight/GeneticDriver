91341f478a1d1cb00a4c0f3aa806a69767578809
Matt Elbé <matt.leboucher@gmail.com>
2018-03-19 22:12:46 -0400

    Slope ranges can be empty.

--- src/Model/Game/Track/Encoding/SlopeRange.java
+++ src/Model/Game/Track/Encoding/SlopeRange.java
@@ -14,6 +14,11 @@ public class SlopeRange {
      */
     private float upperBound;
 
+    /**
+     * Whether or not the interval is empty.
+     */
+    private boolean isEmpty = false;
+
     public SlopeRange(float lowerBound, float upperBound) {
         this.lowerBound = lowerBound;
         this.upperBound = upperBound;
@@ -25,5 +30,13 @@ public class SlopeRange {
 
     public float getUpperBound() {
         return upperBound;
+    }
+
+    public boolean isEmpty() {
+        return isEmpty;
+    }
+
+    public void setEmpty(boolean empty) {
+        isEmpty = empty;
     }
 }
