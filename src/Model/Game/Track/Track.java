14d855c7ddc400b59a6f76b502b716fa4a749979
matt.leboucher <matt.leboucher@gmail.com>
2018-03-19 20:52:54 -0400

    Track class holds segments.

--- src/Model/Game/Track/Track.java
+++ src/Model/Game/Track/Track.java
@@ -1,8 +1,22 @@
 package Model.Game.Track;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author : Matthieu Le Boucher
  */
 public class Track {
+    /**
+     * Ordered list of the segments which constitute the track.
+     */
+    private List<Segment> segments;
 
+    public Track() {
+        this.segments = new ArrayList<Segment>();
+    }
+
+    public void addSegment(Segment segment) {
+        this.segments.add(segment);
+    }
 }
