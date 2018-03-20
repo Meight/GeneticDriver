b210adb9abebff2dec0046c053278b904b52e9e8
Matt Elbé <matt.leboucher@gmail.com>
2018-03-19 23:26:10 -0400

    Check for slopes feseability.

--- src/Model/Game/Track/Encoding/TrackGenerator.java
+++ src/Model/Game/Track/Encoding/TrackGenerator.java
@@ -3,6 +3,7 @@ package Model.Game.Track.Encoding;
 import Model.Game.Track.Segment;
 import Model.Game.Track.Track;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,14 +15,26 @@ public class TrackGenerator {
     public static Track generateTrack(Genotype genotype) {
         Track track = new Track();
 
-        return null;
+        List<SlopeRange> slopeRanges = generateSlopeRanges(genotype.getControlPoints());
+
+        for(SlopeRange slopeRange : slopeRanges)
+            if (slopeRange.isEmpty())
+                track = null;
+
+        return track;
     }
 
     private static Segment generateSegment(ControlPoint p_i, ControlPoint p_j, float slope) {
         return null;
     }
 
-    private List<SlopeRange> generateSlopeRanges(List<ControlPoint> controlPoints) {
-        return null;
+    private static List<SlopeRange> generateSlopeRanges(List<ControlPoint> controlPoints) {
+        List<SlopeRange> slopeRanges = new ArrayList<SlopeRange>();
+
+        for(ControlPoint controlPoint : controlPoints) {
+
+        }
+
+        return slopeRanges;
     }
 }
