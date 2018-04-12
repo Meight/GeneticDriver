47fc9be32a9a737867d2b5e8e59ef88085dccebc
VincentPera <pera@etud.insa-toulouse.fr>
2018-04-12 01:42:10 -0400

    Network Progress

--- src/Model/Network/Client.java
+++ src/Model/Network/Client.java
@@ -1,9 +1,15 @@
 package Model.Network;
 
+import org.aspectj.org.eclipse.jdt.internal.core.SourceType;
+
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -12,21 +18,28 @@ import java.util.List;
 
 public class Client {
     private int port;
-
-    private List<Server> listServer = new ArrayList<>();
-
-    private static final int BUFFER_SIZE = 8192;
-
     private DatagramSocket socket;
+    private List<String> nomServer = new ArrayList<>();
+    private List<InetAddress> addressServer = new ArrayList<>();
+    private static final String SERVERS = "getServers";
+    private static final int BUFFER_SIZE = 8192;
+    private static final int CLIENT_PORT  = 13584;
+    private static final long DELTATIME = 30000;
+    private byte[] buf = new byte[BUFFER_SIZE];
 
-    public Client(int port) {
-        this.port = port;
+    public Client() {
+        try {
+            socket = new DatagramSocket(CLIENT_PORT);
+            this.port = CLIENT_PORT;
+            foundServers();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
     }
 
     public DatagramPacket readPacket() {
-        byte[] buffer = new byte[8192];
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-
+        byte[] buffer = new byte[BUFFER_SIZE];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length); //Todo change logic
         return packet;
     }
 
@@ -38,8 +51,27 @@ public class Client {
         try {
             socket = new DatagramSocket(BUFFER_SIZE);
             socket.setBroadcast(true);
-        } catch (SocketException e) {
+            buf = SERVERS.getBytes();
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            socket.send(packet);
+            long begin = System.currentTimeMillis();
+            while (begin + DELTATIME > System.currentTimeMillis()){
+                socket.receive(packet);
+                InetAddress address = packet.getAddress();
+                addressServer.add(address);
+                String received = new String(packet.getData(), 0, packet.getLength());
+                nomServer.add(received);
+            }
+        } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public void DEBUG_getServers(){
+        for(int i = 0; i < nomServer.size(); ++i){
+            System.out.println("Nom du serveur : "+nomServer.get(i));
+            System.out.println("Adresse du serveur : "+addressServer.get(i));
+            System.out.println();
         }
     }
 }
--- src/Model/Network/Server.java
+++ src/Model/Network/Server.java
@@ -8,37 +8,43 @@ import java.net.InetAddress;
 public class Server extends Thread{
 
     private DatagramSocket socket;
+    private String serverName;
     private boolean running;
-    private byte[] buf = new byte[256];
+    private byte[] buf = new byte[BUFFER_SIZE];
+    private static final int SERVER_PORT  = 13594;
+    private static final int BUFFER_SIZE = 8192;
+    private static final String SERVERS = "getServers";
 
-    public Server() {
+    public Server(String name) {
         try{
-            socket = new DatagramSocket(13594);
+            socket = new DatagramSocket(SERVER_PORT);
+            serverName = name;
         }catch (Exception E){}
     }
 
     public void run() {
         running = true;
-
         while (running) {
             try {
                 DatagramPacket packet = new DatagramPacket(buf, buf.length);
                 socket.receive(packet);
                 InetAddress address = packet.getAddress();
                 int port = packet.getPort();
-                packet = new DatagramPacket(buf, buf.length, address, port);
                 String received = new String(packet.getData(), 0, packet.getLength());
-                if (received.equals("end")) {
-                    running = false;
-                    continue;
+                if(received.equalsIgnoreCase(SERVERS)){
+                    buf = new byte[BUFFER_SIZE];
+                    buf = serverName.getBytes();
+                    packet = new DatagramPacket(buf, buf.length, address, port);
+                    socket.send(packet);
                 }
-                socket.send(packet);
-
-
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
         socket.close();
+    }
+
+    public synchronized void stopServer(){
+        running = false;
     }
 }
