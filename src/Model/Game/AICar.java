400b2020f63a392f5c8a9640857fec396c7a378f
Matt Elbé <matt.leboucher@gmail.com>
2018-04-13 14:56:10 -0400

    Creates AI car class.

---
+++ src/Model/Game/AICar.java
@@ -0,0 +1,12 @@
+package Model.Game;
+
+import org.newdawn.slick.tiled.TiledMap;
+
+/**
+ * @author Matthieu Le Boucher
+ */
+public class AICar extends Car {
+    public AICar(TiledMap map, int x, int y) {
+        super(map, x, y);
+    }
+}
