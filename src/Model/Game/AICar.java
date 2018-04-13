ab93076a854112b2a5e2d724801b72232a3632af
Matt Elbé <matt.leboucher@gmail.com>
2018-04-13 15:10:13 -0400

    Raycasts length made static.

--- src/Model/Game/AICar.java
+++ src/Model/Game/AICar.java
@@ -12,12 +12,12 @@ import org.newdawn.slick.tiled.TiledMap;
  * @author Matthieu Le Boucher
  */
 public class AICar extends Car {
+    private static final double RAYCASTS_LENGTH = 700;
+
     /**
      * List of the directions the AI will constantly check for obstacles using raycasts.
      */
     private Vector2[] checkedDirections = { forward, right, left };
-
-    private double raycastsLength = 700;
 
     public AICar(TiledMap map, int x, int y) {
         super(map, x, y);
@@ -28,7 +28,7 @@ public class AICar extends Car {
         super.render(container, g);
 
         for(Vector2 direction : checkedDirections) {
-            RaycastHit raycastHit = Physics2D.raycast(position, direction, map, raycastsLength);
+            RaycastHit raycastHit = Physics2D.raycast(position, direction, map, RAYCASTS_LENGTH);
 
             if (raycastHit != null) {
                 g.drawLine((int) position.x, (int) position.y,
