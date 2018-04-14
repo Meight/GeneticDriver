284f8ba7a144f44e28546dac1eb0f218a872cb27
VincentPera <pera@etud.insa-toulouse.fr>
2018-04-14 16:50:13 -0400

    Network Boring

--- src/Model/Network/Client.java
+++ src/Model/Network/Client.java
@@ -2,22 +2,28 @@ package Model.Network;
 
 //import org.aspectj.org.eclipse.jdt.internal.core.SourceType;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.*;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * @author Matthieu Le Boucher
  */
 
-public class Client {
+public class Client{
     private int port;
+    static JFrame jFrame;
     private DatagramSocket socket;
-    private List<String> nomServer = new ArrayList<>();
-    private List<InetAddress> addressServer = new ArrayList<>();
+    private List<String> nomServer = new ArrayList<>(0);
+    private HashMap<String,InetAddress> addressServer = new HashMap<>(0);
     private static final String SERVERS = "getServers";
     private static final int BUFFER_SIZE = 8192;
     private static final int CLIENT_PORT  = 13584;
@@ -30,8 +36,8 @@ public class Client {
             socket = new DatagramSocket(CLIENT_PORT);
             socket.setBroadcast(true);
             this.port = CLIENT_PORT;
-            //foundServers();
-        } catch (SocketException e) {
+            foundServers();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -56,9 +62,10 @@ public class Client {
                             e.printStackTrace();
                         }
                         address[0] = packet.getAddress();
-                        addressServer.add(address[0]);
+
                         String received = new String(packet.getData(), 0, packet.getLength());
                         nomServer.add(received);
+                        addressServer.put(received,address[0]);
                     }
                 }
             };
@@ -71,11 +78,53 @@ public class Client {
         }
 
 
-    public void DEBUG_getServers(){
+    public void afficheServers(){
+        jFrame.removeAll();
+        JPanel jPanel = new JPanel(new GridLayout(nomServer.size()+1,1));
+        JButton search = new JButton("Chercher les serveurs");
+        search.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    foundServers();
+                    afficheServers();
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+        jPanel.add(search);
         for(int i = 0; i < nomServer.size(); ++i){
+            JButton go = new JButton(nomServer.get(i));
+            final int fI = i+1;
+            go.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    accessServer(addressServer.get(nomServer.get(fI)));
+                }
+            });
+            jPanel.add(go);
+
             System.out.println("Nom du serveur : "+nomServer.get(i));
-            System.out.println("Adresse du serveur : "+addressServer.get(i));
+            System.out.println("Adresse du serveur : "+addressServer.get(nomServer.get(i)));
             System.out.println();
         }
+        System.out.println("NB Serv : "+nomServer.size());
+        jFrame.add(jPanel);
+        jPanel.validate();
+        jFrame.validate();
+        jFrame.setVisible(true);
+    }
+
+    public void accessServer(InetAddress server){
+        System.out.println("Access server "+server);
+    }
+
+    public void openWindows() {
+        jFrame = new JFrame();
+        jFrame.setTitle("Client Side");
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.setBounds(0, 0, 300, 1000);
+        afficheServers();
     }
 }
--- src/Model/Network/Server.java
+++ src/Model/Network/Server.java
@@ -1,5 +1,9 @@
 package Model.Network;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -8,6 +12,9 @@ import java.net.InetAddress;
 public class Server extends Thread{
 
     private DatagramSocket socket;
+    static JTextField nameServ;
+    static JPanel jPanel;
+    static JFrame jFrame;
     private String serverName;
     private boolean running;
     private byte[] buf = new byte[BUFFER_SIZE];
@@ -15,7 +22,7 @@ public class Server extends Thread{
     private static final int BUFFER_SIZE = 8192;
     private static final String SERVERS = "getServers";
 
-    public Server(String name) {
+    private Server(String name) {
         try{
             socket = new DatagramSocket(SERVER_PORT);
             serverName = name;
@@ -26,7 +33,6 @@ public class Server extends Thread{
         running = true;
         while (running) {
             try {
-                System.out.println("I AM "+serverName);
                 DatagramPacket packet = new DatagramPacket(buf, buf.length);
                 socket.receive(packet);
                 InetAddress address = packet.getAddress();
@@ -47,5 +53,46 @@ public class Server extends Thread{
 
     public synchronized void stopServer(){
         running = false;
+    }
+
+    public static void openWindows() {
+        jFrame = new JFrame();
+        jPanel = new JPanel(new GridLayout(2,1));
+        jFrame.add(jPanel);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.setBounds(0, 0, 500, 250);
+        jFrame.setTitle("Server Side");
+
+        nameServ = new JTextField("Nom de la room");
+        JButton launch = new JButton("Créer la room");
+
+        jPanel.add(nameServ);
+        jPanel.add(launch);
+
+        launch.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkServeur();
+            }
+        });
+
+        jFrame.setVisible(true);
+    }
+
+    private static void checkServeur(){
+        if(nameServ.getText().length() > 0){
+            Server s = new Server(nameServ.getText());
+            s.start();
+            jFrame.remove(jPanel);
+            jPanel.removeAll();
+            JButton cancel = new JButton("Annuler la room");
+            JLabel attente = new JLabel("En attente de l'adversaire ...");
+            jPanel.add(attente);
+            jPanel.add(cancel);
+            jFrame.add(jPanel);
+            jFrame.validate();
+            jPanel.validate();
+            jFrame.setVisible(true);
+        }
     }
 }
