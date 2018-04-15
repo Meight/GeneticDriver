1784fd7359aa672da6051f361fb56ffb3557e050
VincentPera <pera@etud.insa-toulouse.fr>
2018-04-15 14:55:35 -0400

    Change Logic

--- src/Model/Network/OnlineGame.java
+++ src/Model/Network/OnlineGame.java
@@ -16,11 +16,14 @@ public class OnlineGame extends Thread{
     private final static int acceptGame = 13594;
     private final static int portServer = 33334;
     boolean running = true;
+    Thread envoiPacket;
+    Thread recuPaquet;
     static boolean sending = false;
     private static final int BUFFER_SIZE = 8192;
     private static byte[] buf = new byte[BUFFER_SIZE];
     private static byte[] buff = new byte[BUFFER_SIZE];
     private static final String ACCEPT = "go";
+    final DatagramPacket[] packet = {new DatagramPacket(buf, buf.length, server, acceptGame),new DatagramPacket(buff, buff.length)};
 
     public OnlineGame (InetAddress s){
         server = s;
@@ -28,13 +31,54 @@ public class OnlineGame extends Thread{
             socket = new DatagramSocket(portClient+12);
             socketRecept = new DatagramSocket(portClient);
         }catch (Exception E){}
+
+        envoiPacket = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                while (running) {
+                    System.out.println("TRY SEND");
+                    if (sending){
+                        System.out.println("SEND FROM CLIENT");
+                        sending = false;
+                        packet[0] = new DatagramPacket(buf, buf.length, server, portServer);
+                        try {
+                            socket.send(packet[0]);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        buf = new byte[BUFFER_SIZE];
+                    }
+                }
+            }
+        };
+
+        recuPaquet = new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                while(running) {
+                    packet[1] = new DatagramPacket(buff, buff.length);
+                    try {
+                        socketRecept.receive(packet[1]);
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                    String received = new String(packet[1].getData(), 0, packet[1].getLength());
+                    System.out.println("RECU CLIENT : "+received);
+                    //todo received VERS le jeu (nouvelles positions)
+                    buff = new byte[BUFFER_SIZE];
+                }
+            }
+        };
+
     }
 
     public void run() {
         try {
             System.out.println("CLIENT SIDE");
             buf = ACCEPT.getBytes();
-            final DatagramPacket[] packet = {new DatagramPacket(buf, buf.length, server, acceptGame),new DatagramPacket(buff, buff.length)};
+
             socket.send(packet[0]);
             buf = new byte[BUFFER_SIZE];
             try {
@@ -42,51 +86,12 @@ public class OnlineGame extends Thread{
             } catch (SlickException e1) {
                 e1.printStackTrace();
             }
-
-            Thread envoi = new Thread() {
-                @Override
-                public void run() {
-                    super.run();
-                    while (running) {
-                        if (sending){
-                            System.out.println("SEND FROM CLIENT");
-                            sending = false;
-                            packet[0] = new DatagramPacket(buf, buf.length, server, portServer);
-                            try {
-                                socket.send(packet[0]);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                            buf = new byte[BUFFER_SIZE];
-                        }
-                    }
-                }
-            };
-
-            Thread recept = new Thread(){
-                @Override
-                public void run() {
-                    super.run();
-                    while(running) {
-                        packet[1] = new DatagramPacket(buff, buff.length);
-                        try {
-                            socketRecept.receive(packet[1]);
-                        } catch (IOException e1) {
-                            e1.printStackTrace();
-                        }
-                        String received = new String(packet[1].getData(), 0, packet[1].getLength());
-                        System.out.println("RECU CLIENT : "+received);
-                        //todo received VERS le jeu (nouvelles positions)
-                        buff = new byte[BUFFER_SIZE];
-                    }
-                }
-            };
-
-            envoi.start();
-            recept.start();
-                }catch (IOException e1){
-            e1.printStackTrace();
-        }
+            recuPaquet.start();
+            envoiPacket.start();
+            
+            }catch (IOException e1){
+                e1.printStackTrace();
+            }
     }
 
     public static void sendInput(String serial, int delta) {
--- src/Model/Network/ServerGame.java
+++ src/Model/Network/ServerGame.java
@@ -12,6 +12,8 @@ public class ServerGame extends Thread{
     private InetAddress client;
     private final static int portClient = 33333;
     private final static int portServer = 33334;
+    Thread envoiPacket;
+    Thread recuPaquet;
     boolean running = true;
     boolean sending = false;
     private byte[] buf = new byte[BUFFER_SIZE];
@@ -23,59 +25,55 @@ public class ServerGame extends Thread{
             socket = new DatagramSocket(portServer+54);
             socketRecept = new DatagramSocket(portServer);
         }catch (Exception E){}
+        envoiPacket = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                while (running) {
+                    if(sending){
+                        sending = false;
+                        //todo buf prend nouvelle position des deux voitures en Serialized
+                        DatagramPacket packet = new DatagramPacket(buf, buf.length, client, portClient);
+                        try {
+                            socket.send(packet);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        };
+        recuPaquet = new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                while(running) {
+                    System.out.println("RECEIVE FROM SERVER");
+                    buf = new byte[BUFFER_SIZE];
+                    DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                    try {
+                        socketRecept.receive(packet);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    String received = new String(packet.getData(), 0, packet.getLength());
+                    System.out.println("RECU SERVEUR : "+received);
+                    //todo received VERS le jeu (input du joueur distant)
+                    buf = new byte[BUFFER_SIZE];
+                }
+            }
+        };
     }
 
     public void run() {
         try {
             System.out.println("SERVER SIDE");
-        NetworkGame.launch(this);
-
-            Thread envoi = new Thread() {
-                @Override
-                public void run() {
-                    super.run();
-                    while (running) {
-                        if(sending){
-                            sending = false;
-                            //todo buf prend nouvelle position des deux voitures en Serialized
-                            DatagramPacket packet = new DatagramPacket(buf, buf.length, client, portClient);
-                            try {
-                                socket.send(packet);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                }
-            };
-
-            Thread recept = new Thread(){
-                @Override
-                public void run() {
-                    super.run();
-                    while(running) {
-                        System.out.println("RECEIVE FROM SERVER");
-                        buf = new byte[BUFFER_SIZE];
-                        DatagramPacket packet = new DatagramPacket(buf, buf.length);
-                        try {
-                            socketRecept.receive(packet);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        String received = new String(packet.getData(), 0, packet.getLength());
-                        System.out.println("RECU SERVEUR : "+received);
-                        //todo received VERS le jeu (input du joueur distant)
-                        buf = new byte[BUFFER_SIZE];
-                    }
-                }
-            };
-
-            envoi.start();
-            recept.start();
+            NetworkGame.launch(this);
+            recuPaquet.start();
+            envoiPacket.start();
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
     }
 
 }
